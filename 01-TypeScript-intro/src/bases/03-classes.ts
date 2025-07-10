f5f5015bca2b3758224224b025658ebe71416ead
Enrique Irazoqui Ruelas <enrique.irazoqui@carsol.mx>
2025-07-10 09:32:55 -0600

    primera parte del curso completada

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -1,12 +1,40 @@
+import axios from 'axios';
+import type { Move, PokeapiResponse } from '../interfaces/pokeapi-response.interface';
 export class Pokemon{
-    public id: number ;
-    public name: string ;
 
-    constructor( id, name) {
-        this.id = id;
-        this.name = name;
-        console.log('Constructor llamando');
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.jpg`;
     }
-}
 
-export const charmander = new Pokemon(); 
+    constructor(
+        public readonly id: number, 
+        public name: string,
+        // public imageUrl: string,
+       ) {}
+
+        scream() {
+            console.log(`${this.name.toUpperCase()}!!!`);
+       }
+
+        speak() {
+         console.log(`${this.name}, ${this.name}`);
+       }
+
+       async getMoves(): Promise<Move[]> {
+        // const moves = 10;
+        const {data} = await axios.get<PokeapiResponse>('https://pokeapi.co/api/v2/pokemon/4');
+       console.log(data.moves);
+
+        return data.moves;
+        
+       }
+    }
+
+export const charmander = new Pokemon( 2, 'Charmander'); 
+
+// charmander.speak();
+// charmander.scream();
+// console.log(charmander.imageUrl);
+// charmander.id = 10;
+
+console.log(charmander.getMoves());
