7b5aa411ec5e9ef7dde0caf346ba08f126878308
Enrique Irazoqui Ruelas <enrique.irazoqui@carsol.mx>
2025-07-30 12:41:07 -0600

    ultimos cambios del proyecto del pokedex

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -76,8 +76,13 @@ export class PokemonService {
     // await pokemon.deleteOne();
     // return {id}
 
-   const result = this.pokemonModel.findByIdAndDelete(id);
-    return result;
+   const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+
+   if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon with id "${id}" not found`)
+   }
+
+    return ;
   }
 
 
